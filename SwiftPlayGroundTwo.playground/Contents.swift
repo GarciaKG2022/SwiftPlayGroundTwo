6e23f90105189e16356e73e2351039701a946e35
GarciaKG2022 <garciakg@hawaii.edu>
2022-02-09 20:32:27 -0600

    completed on my own portion

--- SwiftPlayGroundTwo.playground/Contents.swift
+++ SwiftPlayGroundTwo.playground/Contents.swift
@@ -8,6 +8,8 @@ var goodGrade = "I've been doing all my work"
 if goodGrade == "I'm not doing very well"{
     //then the code block with execute
     print("I should be getting an A at this point")
+//Part 3
+    
 }else {
     print("I need to work harder in this class")
 }
@@ -20,4 +22,50 @@ if gradeA == true
     print("I really like this class")
 }else{
     print("I'm not so sure about coding in swift")
+}
+
+//Part 5 On My Own
+
+//Problem Set 1
+
+var lil = 9
+var nasX = 2
+
+if lil < nasX
+   {
+    print("Break is over")
+    
+    } else {
+        print("Long ago on the road")
+        
+    }
+
+// Problem Set 2
+
+var avri = "He was a skater boy "
+var lavi = "She said see you later boy"
+
+if avri == "He was a skater boy"
+{
+    print(avri + lavi)
+    
+}
+
+// Problem Set 3
+
+var sia = false
+var ti = false
+
+if sia == true
+{
+    print("I've got thick skin")
+}else{
+    print("I've got an elastic heart.")
+}
+
+if ti == true
+{
+    print("fire away")
+}else{
+    print("I am titanium")
 }
